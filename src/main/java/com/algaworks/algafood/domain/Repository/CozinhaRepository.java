0453d85a2cc14d970e7a407a3231989c4f5b7216
wilian.souza <wiliansouza.zetta@ufla.br>
2021-06-21 23:32:11 -0300

    Implementação de uma consulta por nome usando JPA.

--- src/main/java/com/algaworks/algafood/domain/Repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/Repository/CozinhaRepository.java
@@ -4,6 +4,11 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+
+    Optional<Cozinha> findByNome(String nome);
+
 }
