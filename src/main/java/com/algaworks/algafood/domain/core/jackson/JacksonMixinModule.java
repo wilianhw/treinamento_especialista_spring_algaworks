be4d36438f5b7008627e68e38d4adef097f1961f
wilian.souza <wiliansouza.ti@fundecc.org.br>
2021-11-05 23:16:20 -0300

    Criando e vinculando class mixin.

---
+++ src/main/java/com/algaworks/algafood/domain/core/jackson/JacksonMixinModule.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.domain.core.jackson;
+
+import com.algaworks.algafood.api.mixin.CidadeMixin;
+import com.algaworks.algafood.api.mixin.CozinhaMixin;
+import com.algaworks.algafood.api.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JacksonMixinModule extends SimpleModule {
+
+    public JacksonMixinModule() {
+        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
+        setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+    }
+}
