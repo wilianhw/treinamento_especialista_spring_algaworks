2ebb8f64661061d8e55571fda6a759cc1c7d6e3f
wilian.souza <wiliansouza.ti@fundecc.org.br>
2022-03-12 14:29:51 -0300

    :construction: Comentado uso de grupos das classes.

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,11 +1,9 @@
 package com.algaworks.algafood.domain.model;
 
-import com.algaworks.algafood.api.core.validation.Groups;
 import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +11,7 @@ import java.util.List;
 @Data
 public class Cozinha {
 
-    @NotNull(groups = Groups.CozinhaId.class)
+//    @NotNull(groups = Groups.CozinhaId.class)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
