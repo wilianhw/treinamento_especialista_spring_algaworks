d34a04b0fa6993c5a46d9a1dae583113182bddaa
wilian.souza <wiliansouza.ti@fundecc.org.br>
2022-03-12 14:29:15 -0300

    :construction: Implementado jackson mixin.

--- src/main/java/com/algaworks/algafood/domain/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/domain/core/jackson/JacksonMixinModule.java
@@ -2,10 +2,8 @@ package com.algaworks.algafood.domain.core.jackson;
 
 import com.algaworks.algafood.api.mixin.CidadeMixin;
 import com.algaworks.algafood.api.mixin.CozinhaMixin;
-import com.algaworks.algafood.api.mixin.RestauranteMixin;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +11,6 @@ import org.springframework.stereotype.Component;
 public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
-        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
         setMixInAnnotation(Cidade.class, CidadeMixin.class);
         setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
     }
