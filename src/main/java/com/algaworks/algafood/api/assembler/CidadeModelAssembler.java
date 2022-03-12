1d29b19276c602f5538acb6b83ab419a022f73be
wilian.souza <wiliansouza.ti@fundecc.org.br>
2022-03-12 14:25:54 -0300

    :construction: Criação dos Assemblers.

---
+++ src/main/java/com/algaworks/algafood/api/assembler/CidadeModelAssembler.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood.api.assembler;
+
+import com.algaworks.algafood.api.model.CidadeModel;
+import com.algaworks.algafood.domain.model.Cidade;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CidadeModelAssembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public CidadeModel toModel(Cidade cidade) {
+        return modelMapper.map(cidade, CidadeModel.class);
+    }
+}
