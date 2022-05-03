7e844905741e73eb3a4258ad6fce94c6a26a320a
Wilian <wilian.hw@hotmail.com>
2022-05-02 21:45:58 -0300

    Criação de novos DTO's.

--- src/main/java/com/algaworks/algafood/domain/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/domain/core/modelmapper/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.algaworks.algafood.domain.core.modelmapper;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,14 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        TypeMap<Endereco, EnderecoModel> enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoDest, value) -> enderecoDest.getCidade().setEstado(value));
+
+        return modelMapper;
     }
 }
